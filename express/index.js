b17ede8fb34f83be88ef7cf1d8865caf9bd2dcc1
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2024-06-29 00:42:05 +0100

    express underway

--- express/index.js
+++ express/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import * as cheerio from 'cheerio'
+
 const app = express()
 const port = 3001
 
@@ -8,15 +10,50 @@ app.get('/', (req, res) => {
 
 app.get('/BigChungus', async (req, res) => {
   const getData = async () => {
-    const response = await fetch('https://apod.nasa.gov/apod/ap240622.html')
+    const response = await fetch('https://apod.nasa.gov/apod/ap240627.html')
     const data = await response.text()
-    const imgRegex = /<img[^>]+src="([^">]+)"/g
-    let imgResult = []
-    let match
-    while ((match = imgRegex.exec(data)) !== null) {
-      imgResult.push(match[1])
+    console.log('data:', data)
+    const imgRegex = /<img[^>]+src="([^">]+)"/gi
+    const iframeRegex = /<iframe[^>]+src="([^">]+)"/gi
+    const explanationRegex = /<b> Explanation: <\/b>(.*?)<p>/is
+    const titleRegex = /<title>\s*(.*?)\s*<\/title>/i
+    const date = '1'
+    let urlMatch
+    let titleMatch
+
+    let resultObj = { date: date }
+    while ((urlMatch = imgRegex.exec(data)) !== null) {
+      resultObj.hdurl = urlMatch[1]
+      resultObj.media_type = 'image'
     }
-    return imgResult
+    while ((urlMatch = iframeRegex.exec(data)) !== null) {
+      resultObj.hdurl = urlMatch[1]
+      resultObj.media_type = 'video'
+    }
+    titleMatch = titleRegex.exec(data)
+    if (titleMatch !== null) {
+      resultObj.title = titleMatch[1].trim()
+    }
+    console.log(titleMatch)
+
+    let explanationMatch = explanationRegex.exec(data)
+    if (explanationMatch !== null) {
+      const $ = cheerio.load(data)
+
+      const explanationElement = $('b:contains("Explanation:")').parent()
+
+      explanationElement.find('a').replaceWith((_, elem) => $(elem).text())
+      explanationElement.find('br').replaceWith(' ')
+      explanationElement.find('p').replaceWith(' ')
+
+      let explanationText = explanationElement.text().replace('Explanation: ', '').trim()
+      explanationText = explanationText.replace(/\n/g, ' ')
+
+      resultObj.explanation = explanationText
+    }
+    console.log(resultObj)
+
+    return resultObj
   }
   const finalData = await getData()
 
