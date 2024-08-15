9747502f720840fca4557b83df83a816b4add364
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2024-08-15 20:33:29 +0100

    soon to be desc update

---
+++ src/utils/DescriptionDateConversion.tsx
@@ -0,0 +1,4 @@
+export const descriptionDateConversion = () => {}
+
+//menus not opening shrunk/ mobile
+//desc background transparent, behind text black background with box shadow that fades from black to transparent
