da18af9595639e6fbc169cabf670d668400c8d5b
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2024-12-10 22:04:31 +0000

    fixed december month

--- src/utils/toSixFigureDate.tsx
+++ src/utils/toSixFigureDate.tsx
@@ -1,5 +1,12 @@
 export const toSixFigureDate = (year: number, month: number, day: number) => {
-  return `${year.toString().slice(-2).padStart(2, '0')}${month.toString().padStart(2, '0')}${day
+  let yearModified = year
+  let monthModified = month
+  let dayModified = day
+  if (month === 0) {
+    monthModified = 12
+    yearModified -= 1
+  }
+  return `${yearModified.toString().slice(-2).padStart(2, '0')}${monthModified.toString().padStart(2, '0')}${dayModified
     .toString()
     .padStart(2, '0')}`
 }
