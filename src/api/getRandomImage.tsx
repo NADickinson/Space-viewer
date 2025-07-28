f39e9700e58eb52447b62a18b5ff03045967eb00
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2025-07-28 18:33:04 +0100

    random button disable

--- src/api/getRandomImage.tsx
+++ src/api/getRandomImage.tsx
@@ -1,13 +1,13 @@
-export const getRandomImage = async () => {
-  try {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}RandomImage`)
-    if (!response.ok) {
-      throw new Error('response error')
-    }
+// export const getRandomImage = async () => {
+//   try {
+//     const response = await fetch(`${process.env.REACT_APP_API_URL}RandomImage`)
+//     if (!response.ok) {
+//       throw new Error('response error')
+//     }
 
-    const data = await response.json()
-    return data
-  } catch (err) {
-    console.log(err)
-  }
-}
+//     const data = await response.json()
+//     return data
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
